5d247ccf2bc4d961140da41df15c84d30451e3de
Durham36 <octavius.kunz@gmail.com>
2025-04-23 11:23:45 -0600

    Making another file.
    Making another file to import into the main one.
    this file contains the monster rencounters.

---
+++ Mistwood Encounters.py
@@ -0,0 +1,18 @@
+Mistwood_encounters = [
+  [
+         {"name": "Mistwood Spider", "health": 2, "min_attack": 0, "max_attack": 1, "gold_drop": 1},
+         {"name": "Mistwood Spider", "health": 2, "min_attack": 0, "max_attack": 1, "gold_drop": 1}
+     ],
+   
+     [
+          {"name": "Mistwood Spider", "health": 2, "min_attack": 0, "max_attack": 1, "gold_drop": 1},
+          {"name": "Mistwood Spider", "health": 2, "min_attack": 0, "max_attack": 1, "gold_drop": 1},
+          {"name": "Mistwood Spider", "health": 2, "min_attack": 0, "max_attack": 1, "gold_drop": 1}
+     ],
+     
+     [
+          {"name": "Mistwood Spider", "health": 2, "min_attack": 0, "max_attack": 1, "gold_drop": 1},
+          {"name": "Mistwood Spider", "health": 2, "min_attack": 0, "max_attack": 1, "gold_drop": 1},
+          {"name": "Mistwood Ooze", "health": 3, "min_attack": 0, "max_attack": 2, "gold_drop": 2}
+     ]
+  ]
